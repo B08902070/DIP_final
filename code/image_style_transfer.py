b7a49bd68a003c04dd3de29b7afd6ebd75f65853
B08902070 <72483821+B08902070@users.noreply.github.com>
2023-01-02 14:55:13 +0800

    fix path

--- code/image_style_transfer.py
+++ code/image_style_transfer.py
@@ -15,33 +15,36 @@ def make_temp_dir(dir_name, img, img_name):
     os.makedirs(dir_name)
     cv2.imwrite(f"{dir_name}/{img_name}.jpg", img)
 
-def run_style_transfer_algo(algo, content_img, style_img, content_name, style_name, output_dir):
-    if algo == 'LinearStyleTransfer':
+def run_style_transfer_algo(args, content_img, style_img, content_name, style_name):
+    if args.nst_algo == 'LinearStyleTransfer':
         tmp_content_dir = "tmp_content"
         tmp_style_dir = "tmp_style"
         make_temp_dir(tmp_content_dir, content_img, content_name)
         make_temp_dir(tmp_style_dir, style_img, style_name)
         
         subprocess.run(['python', 'LinearStyleTransfer/TestArtistic.py', '--contentPath', tmp_content_dir, '--stylePath', tmp_style_dir,
-                        '--outf', str(output_dir)])
+                        '--outf', args.output_dir])
 
-    elif algo == "Gatys":
-        subprocess.run(['Gatys/style_transfer', f'{tmp_content_dir}', 
-                        f'{tmp_style_dir}', '-o', f'{str(output_dir)}/{content_name}_{style_name}.jpg'])
+    elif args.nst_algo == "Gatys":
+        tmp_content_path = args.content_dir + f'{content_name}.jpg'
+        tmp_style_path = args.style_dir + f'{style_name}.jpg'
 
-    elif algo == "MCCNet":
-        tmp_content_path = f'{content_name}.jpg'
-        tmp_style_path = f'{style_name}.jpg'
+        subprocess.run(['Gatys/style_transfer', tmp_content_path, 
+                        tmp_style_path, '-o', f'{args.output_dir}{content_name}_{style_name}.jpg'])
+
+    elif args.nst_algo == "MCCNet":
+        tmp_content_path = args.content_dir + f'{content_name}.jpg'
+        tmp_style_path = args.style_dir + f'{style_name}.jpg'
 
         subprocess.run(['python', 'MCCNet/test_video.py', '--content', tmp_content_path, 
-                        '--style', tmp_style_path, '--output', str(output_dir)])
+                        '--style', tmp_style_path, '--output', args.output_dir])
 
-    elif algo == "SANet":
-        tmp_content_path = f'{content_name}.jpg'
-        tmp_style_path = f'{style_name}.jpg'
+    elif args.nst_algo == "SANet":
+        tmp_content_path = args.content_dir + f'{content_name}.jpg'
+        tmp_style_path = args.style_dir + f'{style_name}.jpg'
 
         subprocess.run(['python', 'SANet/Eval.py', '--content', tmp_content_path,
-                        '--style', tmp_style_path, '--output', str(output_dir)])
+                        '--style', tmp_style_path, '--output', args.output_dir])
         
 
 
@@ -75,13 +78,13 @@ def image_style_transfer(args):
         for si in range(len(style_imgs)):
             """ if content-style pair not in command, then do nothing before and after style transfer """
             if config_dict is None or content_names[ci] not in config_dict.keys():
-                run_style_transfer_algo(args.nst_algo, content_img=content_imgs[ci], style_img=style_imgs[si],
-                                        content_name=content_names[ci], style_name=style_names[si], output_dir=output_dir)
+                run_style_transfer_algo(args, content_img=content_imgs[ci], style_img=style_imgs[si],
+                                        content_name=content_names[ci], style_name=style_names[si])
                 continue
             content_dict = config_dict[content_names[ci]]
             if style_names[si] not in content_dict.keys():
-                run_style_transfer_algo(args.nst_algo, content_img=content_imgs[ci], style_img=style_imgs[si],
-                                        content_name=content_names[ci], style_name=style_names[si], output_dir=output_dir)
+                run_style_transfer_algo(args, content_img=content_imgs[ci], style_img=style_imgs[si],
+                                        content_name=content_names[ci], style_name=style_names[si])
                 continue
 
             
@@ -104,8 +107,8 @@ def image_style_transfer(args):
             content_img = RT_console.forward(content_imgs[ci])
             
             """do image style transfer"""
-            run_style_transfer_algo(args.nst_algo, content_img=content_imgs[ci], style_img=style_imgs[si],
-                                    content_name=content_names[ci], style_name=style_names[si], output_dir=output_dir)
+            run_style_transfer_algo(args, content_img=content_imgs[ci], style_img=style_imgs[si],
+                                    content_name=content_names[ci], style_name=style_names[si])
             
             stylized_path = list(output_dir.glob(f'{content_names[ci]}_{style_names[si]}*'))[0]
             stylized_img = cv2.imread(stylized_path)
